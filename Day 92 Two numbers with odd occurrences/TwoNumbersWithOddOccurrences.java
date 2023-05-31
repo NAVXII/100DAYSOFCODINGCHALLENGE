d2db1b161b5100fb6980c59939dc75f8e5aec0ff
Naveen Bara <107382587+NAVXII@users.noreply.github.com>
2023-05-31 23:08:18 +0530

    Create TwoNumbersWithOddOccurrences.java

---
+++ Day 92 Two numbers with odd occurrences/TwoNumbersWithOddOccurrences.java
@@ -0,0 +1,50 @@
+import java.io.*;
+import java.util.*;
+
+class GFG{
+    public static void main(String args[])throws IOException
+    {
+        BufferedReader read = new BufferedReader(new InputStreamReader(System.in));
+        int t = Integer.parseInt(read.readLine());
+        while(t-- > 0)
+        {
+            int N = Integer.parseInt(read.readLine());
+            String input_line[] = read.readLine().trim().split("\\s+");
+            int Arr[]= new int[N];
+            for(int i = 0; i < N; i++)
+                Arr[i] = Integer.parseInt(input_line[i]);
+            
+            Solution ob = new Solution();
+            int[] ans = ob.twoOddNum(Arr,N);
+            for(int i = 0; i < ans.length; i++)
+            {
+                System.out.print(ans[i] + " ");
+            }
+            System.out.println();
+        }
+    }
+}
+// } Driver Code Ends
+
+
+//User function Template for Java
+
+//User function Template for Java
+class Solution
+{
+    public int[] twoOddNum(int Arr[], int N)
+    {
+        int[] ans = new int[2];
+        Map<Integer,Integer> map = new TreeMap<>();
+        for(int i : Arr) {
+            map.put(i,map.getOrDefault(i,0)+1);
+        }
+        int ind = 1;
+        for(int i : map.keySet()) {
+            if(map.get(i) % 2 != 0) {
+                ans[ind--] = i;
+            }
+        }
+        return ans;
+    }
+}
