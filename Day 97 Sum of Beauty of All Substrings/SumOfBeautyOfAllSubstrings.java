6dfa69337ff7e4e8f4f56e1e056159018138f641
Naveen Bara <107382587+NAVXII@users.noreply.github.com>
2023-06-01 23:05:31 +0530

    Create SumOfBeautyOfAllSubstrings.java

---
+++ Day 97 Sum of Beauty of All Substrings/SumOfBeautyOfAllSubstrings.java
@@ -0,0 +1,47 @@
+import java.io.*;
+import java.util.*;
+
+class GFG {
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        int t;
+        t = Integer.parseInt(br.readLine());
+        while(t-- > 0){
+            //br.readLine();
+            String s;
+            s = br.readLine();
+            
+            Solution obj = new Solution();
+            int res = obj.beautySum(s);
+            
+            System.out.println(res);
+            
+        }
+    }
+}
+
+// } Driver Code Ends
+
+
+class Solution {
+    public static int beautySum(String s) {
+          int ans = 0;
+        int n = s.length();
+        for (int i = 0; i < n; ++i) {
+            int[] counter = new int[26];
+            for (int j = i; j < n; ++j) {
+                ++counter[s.charAt(j) - 'a'];
+                int mi = 1000;
+                int mx = 0;
+                for (int v : counter) {
+                    if (v > 0) {
+                        mi = Math.min(mi, v);
+                        mx = Math.max(mx, v);
+                    }
+                }
+                ans += mx - mi;
+            }
+        }
+        return ans;
+    }
+}
